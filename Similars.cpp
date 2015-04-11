9dc5ef2933c20cfc91f67fc2167d4bdaae33123d
ry0u <ry0u_yd@yahoo.co.jp>
2015-04-12 04:43:36 +0900

    update

--- Similars.cpp
+++ Similars.cpp
@@ -16,34 +16,6 @@
 
 using namespace std;
 
-int bitcnt(int A,int B)
-{
-	stringstream ss;
-	ss << A;
-	string a = ss.str();
-
-	stringstream ss2;
-	ss2 << B;
-	string b = ss2.str();
-
-	int ret = 0;
-	rep(i,a.size())
-	{
-		ret |= (1 << (a[i]-'0'));
-	}
-
-	int ret2 = 0;
-	rep(i,b.size())
-	{
-		ret2 |= (1 << (b[i]-'0'));
-	}
-
-	ret &= ret2;
-	bitset<10> bit(ret);
-	
-	return bit.count();
-}
-
 int tobit(int A)
 {
 	stringstream ss;
@@ -61,15 +33,13 @@ class Similars {
 	int maxsim(int L, int R) {
 		
 		stringstream ss,ss2;
-		ss << L;
-		int l = ss.str().size();
-
+		
 		ss2 << R;
 		int r = ss2.str().size();
 
 		int ans = 0;
 
-		bool used[1050];
+		bool used[1050],used2[1050];
 		memset(used,0,sizeof(used));
 
 		for(int i=R;i>=L;i--)
@@ -78,7 +48,6 @@ class Similars {
 			if(used[A]) continue;
 			used[A] = true;
 
-			bool used2[1050];
 			memset(used2,0,sizeof(used2));
 
 			for(int j=i-1;j>=L;j--)
@@ -87,16 +56,15 @@ class Similars {
 				if(used2[B]) continue;
 				used2[B] = true;
 
-				int d = bitcnt(i,j);
+				int d = __builtin_popcount(A&B);
 				ans = max(ans,d);
 			}
 
 			stringstream ss;
 			ss << i;
-
 			int size = ss.str().size();
 			
-			if(ans == size || size < r-1) break;
+			if(size < r-1) break;
 			if(size <= ans) break;
 		}
 
